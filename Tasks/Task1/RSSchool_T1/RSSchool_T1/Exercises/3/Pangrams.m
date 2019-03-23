04881f5722f0f9b0c7d4b84100236a80ff860810
Alex Kufel <kufelalexander@gmail.com>
2019-03-23 12:45:09 +0300

    added pangrams function

--- Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
+++ Tasks/Task1/RSSchool_T1/RSSchool_T1/Exercises/3/Pangrams.m
@@ -3,8 +3,26 @@
 @implementation Pangrams
 
 // Complete the pangrams function below.
-- (BOOL)pangrams:(NSString *)string {    
-    return NO;
+- (BOOL)pangrams:(NSString *)string {
+    NSString *pangrams = @"";
+    NSString *lineOfCharacters = [string stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSString *lowercaseString = [lineOfCharacters lowercaseString];
+
+    for (int i = 0; i <= lowercaseString.length; i++) {
+        NSString *abc = [lowercaseString substringToIndex:i];
+        if (abc.length > 0) {
+            abc = [abc substringFromIndex:i - 1];
+        }
+        if ([pangrams containsString:abc]) {
+        } else  {
+            pangrams = [pangrams stringByAppendingString:abc];
+        }
+    }
+    if (pangrams.length == 26) {
+        return YES;
+    } else {
+        return NO;
+    }
 }
 
 @end
