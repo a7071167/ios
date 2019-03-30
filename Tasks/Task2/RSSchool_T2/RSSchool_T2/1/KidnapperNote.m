f6c6f8ccbcaee32e75de93cce158dd16c602fb6c
Alex Kufel <kufelalexander@gmail.com>
2019-03-30 19:36:55 +0300

    Added CheckMagazine function

--- Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
+++ Tasks/Task2/RSSchool_T2/RSSchool_T2/1/KidnapperNote.m
@@ -1,5 +1,28 @@
 #import "KidnapperNote.h"
 
 @implementation KidnapperNote
-// your code here
+
+- (BOOL)checkMagazine:(NSString *)magaine note:(NSString *)note {
+    NSInteger counter = 0;
+    NSArray *magazines = [[magaine lowercaseString] componentsSeparatedByString:@" "];
+    NSArray *notes = [[note lowercaseString] componentsSeparatedByString:@" "];
+    
+    NSMutableArray *magazineArray = [[NSMutableArray alloc] initWithArray:magazines];
+    
+    for (NSString *strNote in notes) {
+        for (NSString *strMagazine in magazineArray) {
+            if ([strNote isEqualToString:strMagazine]) {
+                [magazineArray removeObject:strMagazine];
+                counter++;
+                break;
+            }
+        }
+    }
+    if (counter == notes.count) {
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
 @end
